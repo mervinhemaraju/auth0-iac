9f5babd0cfecf005364c20d83e2d4b03fa7402a1
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-07-25 23:11:58 +0400

    - Added an adminsitrator user

--- users.tf
+++ users.tf
@@ -1,8 +1,13 @@
 
-# resource "auth0_user" "administrator" {
-#   connection_name = auth0_connection.managed_users.name
-#   email           = "mervinhemaraju16@gmail.com"
-#   username        = "th3pl4gu3"
-#   password        = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATOR_PASSWORD
-#   email_verified  = true
-# }
+resource "auth0_user" "administrator" {
+  connection_name = auth0_connection.managed_users.name
+  email           = "mervinhemaraju16@gmail.com"
+  username        = "th3pl4gu3"
+  password        = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATOR_PASSWORD
+  email_verified  = true
+
+  depends_on = [
+    auth0_client.cloudflare_zta,
+    auth0_connection.managed_users
+  ]
+}
