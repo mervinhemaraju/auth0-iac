1afb0bbabc0705000710b4a5ba6f6e943a463917
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-07-25 18:19:11 +0400

    - Fixed issue in auth0 provider creation

--- providers.tf
+++ providers.tf
@@ -6,6 +6,6 @@ provider "doppler" {
 # Auth0 Provider Configuration
 provider "auth0" {
   domain        = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATIVE_DOMAIN
-  client_id     = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATIVE_ID
+  client_id     = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATIVE_CLIENT_ID
   client_secret = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATIVE_CLIENT_SECRET
 }
