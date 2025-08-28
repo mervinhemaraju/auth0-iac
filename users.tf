92a9c94b38b41e5c1f2623bbb4f9c8fb11fed1b5
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-08-28 18:48:51 +0400

    - Fixed issue in secrets referencing

--- users.tf
+++ users.tf
@@ -3,7 +3,7 @@ resource "auth0_user" "administrator" {
   connection_name = auth0_connection.managed_users.name
   email           = "mervinhemaraju16@gmail.com"
   username        = "th3pl4gu3"
-  password        = data.doppler_secrets.auth0.map.AUTH0_ADMINISTRATOR_PASSWORD
+  password        = data.doppler_secrets.auth0_creds.map.AUTH0_ADMINISTRATOR_PASSWORD
   email_verified  = true
 
   depends_on = [
