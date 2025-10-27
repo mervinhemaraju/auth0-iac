2f066ae44c4cbc596b8e0caa2302cac5acca9651
Mervin Hemaraju <th3pl4gu33@gmail.com>
2025-10-27 12:14:15 +0400

    - Created a new secret to store the argocd plagueworks app secrets

--- secrets.tf
+++ secrets.tf
@@ -11,3 +11,17 @@ resource "doppler_secret" "cloudflare_zta_config" {
     }
   )
 }
+
+# Create a secret for the argocd plagueworks app config
+resource "doppler_secret" "argocd_plagueworks_config" {
+  project    = local.secrets.auth0
+  config     = "prd"
+  name       = "AUTH0_APPS_ARGOCD_PLAGUEWORKS_CONFIG"
+  value_type = "json"
+  value = jsonencode(
+    {
+      "application_id" : auth0_client.argocd_plagueworks.client_id
+      "application_secret" : auth0_client_credentials.argocd_plagueworks.client_secret
+    }
+  )
+}
